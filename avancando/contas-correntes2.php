315bab633579266567b50b881b6a49a218167f79
AlbertMelo <aldm.cid20@uea.edu.br>
2021-11-13 22:32:07 -0300

    Avancando no PHP - Refinando

--- avancando/contas-correntes2.php
+++ avancando/contas-correntes2.php
@@ -1,28 +1,23 @@
 <?php
-$conta1 = [
-    'titular' => 'Vinicius',
-    'saldo' => 1000
-];
-
-$conta2 = [
-    'titular' => 'Maria',
-    'saldo' => 500
-];
-
-$conta3 = [
-    'titular' => 'Claudio',
-    'saldo' => 1500
-];
 
 $contasCorrentes = [
-    12345679810 => $conta1,
-    12346579811 => $conta2,
-    12345678912 => $conta3
+    12345678910 => [
+        'titular' => 'Vinicius',
+        'saldo' => 1000
+    ],
+    12345678911 => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    12325678912 => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ]
 ];
 
 // Listando com foreach
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $conta['titular'] . " - " . $conta['saldo'] . PHP_EOL;
+    echo $cpf . " - " . $conta['titular'] . " - " . $conta['saldo'] . PHP_EOL;
 }
 
 
