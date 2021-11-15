ee90295f759f4b3db145b193587c8f0cb71c1733
AlbertMelo <aldm.cid20@uea.edu.br>
2021-11-15 11:30:20 -0300

    Avancando no PHP - Funcoes

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,4 +1,7 @@
 <?php
+
+include 'funcoes.php';
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
@@ -17,6 +20,5 @@ $contasCorrentes = [
 $contasCorrentes['123.456.789-10']['saldo'] -= 500;
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . " " . $conta['titular'] . ' ' . $conta['saldo'] . PHP_EOL;
+    echo exibeMensagem("$cpf  {$conta['titular']} {$conta['saldo']}");
 }
-;
