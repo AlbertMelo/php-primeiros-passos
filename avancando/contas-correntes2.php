b2255f15a8df8029023929f35b9a868f403e1aa6
AlbertMelo <aldm.cid20@uea.edu.br>
2021-11-15 10:51:50 -0300

    Banco - Introdução de Funções

--- avancando/contas-correntes2.php
+++ avancando/contas-correntes2.php
@@ -1,19 +1,34 @@
 <?php
-
 $contasCorrentes = [
-    12345678910 => [
+    '123.456.789-10' => [
         'titular' => 'Vinicius',
         'saldo' => 1000
     ],
-    12345678911 => [
+    
+    '123.456.789-11' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    12325678912 => [
+    
+    '123.256.789-12' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ]
 ];
+
+
+//Adicionando um novo item 
+
+$contasCorrentes['132.456.789-13'] = [
+    'titular' => 'Claudia',
+    'saldo' => 2000
+];
+
+$contasCorrentes['123.456.789-14'] = [
+    'titular' => 'Claudia 2',
+    'saldo' => 2000
+];
+
 
 // Listando com foreach
 foreach ($contasCorrentes as $cpf => $conta) {
